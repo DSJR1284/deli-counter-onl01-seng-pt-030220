06c1eedfbf37c06427e6a1180071cc2a0eee25fc
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-11 04:59:14 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,8 @@ def line(katz_deli)
   def now_serving(katz_deli)
     if katz_deli.length == 0 
       puts "There is nobody waiting to be served!"
-   else 
+   else
+     puts
    end 
  end
   
