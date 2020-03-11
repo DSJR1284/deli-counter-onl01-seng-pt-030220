005e6014559743ff1bb0ddbaef02947f5499b1a4
beloved-scraper-9319 <dsjr1284@gmail.com>
2020-03-11 04:50:55 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,26 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli)
+  cus_line = []
+  if katz_deli.length == 0 
+    puts "The line is currently empty."
+    else
+      num = 1 
+      katz_deli.each do |cus|
+    cus_line << "#{num}. #{cus}"
+      num += 1 
+    end 
+    puts "The line is currently: #{cus_line.join(" ")}"
+  end 
+ end 
+ 
+ def take_a_number(katz_deli, name)
+    katz_deli << name 
+   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
+  end
+  
+  def now_serving(katz_deli)
+    if 
+    
+  end 
+  
